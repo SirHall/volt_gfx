6b7c02e45ddfffc51dc28577a3d85dd3fcec99b5
SirHall <sirhallstein@gmail.com>
2019-10-28 23:40:45 +1000

    Add ShaderSource which handles grouping shadelets together into one combined group of shadelets

---
+++ volt_gfx/include/volt/gfx/ShaderSource.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#ifndef ShaderSource_hpp
+#define ShaderSource_hpp
+
+#include "ShadeletSource.hpp"
+
+#include <string>
+#include <vector>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+namespace volt::gfx
+{
+
+    class ShaderSource
+    {
+    private:
+        std::string                 name;
+        std::vector<ShadeletSource> shadelets;
+
+        static bool HasEnding(std::string const &fullString,
+                              std::string const &ending);
+
+        static bool GetShaderType(std::string const &typeString, GLenum &type);
+
+    public:
+        ShaderSource(std::string name);
+        ~ShaderSource();
+
+        const std::string &GetName() const;
+
+        static std::vector<ShaderSource>
+            ReadShaderSources(std::string directoryPath);
+
+        void AddShadelet(ShadeletSource shadelet);
+
+        std::vector<ShadeletSource> const &GetShadelets() const;
+    };
+} // namespace volt::gfx
+#endif
