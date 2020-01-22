240e01a85789120013df188f0e6e98ea4cf90963
SirHall <sirhallstein@gmail.com>
2020-01-22 18:23:35 +1000

    Add GFXEvents header to include all volt_gfx events

---
+++ volt_gfx/include/volt/gfx/global_events/GFXEvents.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef VOLT_GFX_GLOBAL_EVENTS_GFXEVENTS_HPP
+#define VOLT_GFX_GLOBAL_EVENTS_GFXEVENTS_HPP
+
+#include "volt/gfx/global_events/GFXEventChar.hpp"
+#include "volt/gfx/global_events/GFXEventError.hpp"
+#include "volt/gfx/global_events/GFXEventKey.hpp"
+
+#endif
