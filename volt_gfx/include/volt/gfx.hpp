91c17051fce46d2d81773d143f612c4b96792f5e
SirHall <sirhallstein@gmail.com>
2020-01-22 18:25:18 +1000

    Add volt/gfx.hpp header to include entire library

---
+++ volt_gfx/include/volt/gfx.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef VOLT_GFX_HPP
+#define VOLT_GFX_HPP
+
+#include "volt/gfx/GLUtilities.hpp"
+#include "volt/gfx/Material.hpp"
+#include "volt/gfx/Mesh.hpp"
+#include "volt/gfx/Renderer.hpp"
+#include "volt/gfx/ShadeletSource.hpp"
+#include "volt/gfx/Shader.hpp"
+#include "volt/gfx/ShaderSource.hpp"
+#include "volt/gfx/Sprite.hpp"
+#include "volt/gfx/Texture.hpp"
+#include "volt/gfx/Transform.hpp"
+#include "volt/gfx/Vertex.hpp"
+#include "volt/gfx/attribute/AttributeConstructor.hpp"
+#include "volt/gfx/global_events/GFXEvents.hpp"
+
+#endif
