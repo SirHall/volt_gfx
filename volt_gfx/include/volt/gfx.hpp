2ea4d1b4188f14e0f337c0011cc8e2bd26c062ae
SirHall <sirhallstein@gmail.com>
2020-02-11 16:01:07 +1000

    Add GLImport to properly handle importing glfw and gl headers

--- volt_gfx/include/volt/gfx.hpp
+++ volt_gfx/include/volt/gfx.hpp
@@ -2,6 +2,7 @@
 #ifndef VOLT_GFX_HPP
 #define VOLT_GFX_HPP
 
+#include "volt/gfx/GLImport.hpp"
 #include "volt/gfx/GLUtilities.hpp"
 #include "volt/gfx/Material.hpp"
 #include "volt/gfx/Mesh.hpp"
@@ -13,7 +14,6 @@
 #include "volt/gfx/Texture.hpp"
 #include "volt/gfx/Transform.hpp"
 #include "volt/gfx/Vertex.hpp"
-#include "volt/gfx/attribute/AttributeConstructor.hpp"
 #include "volt/gfx/global_events/GFXEvents.hpp"
 
 #endif
---
+++ volt_gfx/include/volt/gfx/GLImport.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef VOLT_GFX_GLIMPORT_HPP
+#define VOLT_GFX_GLIMPORT_HPP
+
+// OpenGL Start
+#include <volt/gl.hpp>
+// glew must be imported before glfw3
+#include <GLFW/glfw3.h>
+// OpenGL End
+
+#endif
