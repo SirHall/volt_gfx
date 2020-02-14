2650c7ec08b3bfd3321116e0929bd376e11c75d5
SirHall <sirhallstein@gmail.com>
2020-02-14 17:11:22 +1000

    Allow textures to be loaded from storage and loaded into VRAM

--- volt_gfx/include/volt/gfx/Texture.hpp
+++ volt_gfx/include/volt/gfx/Texture.hpp
@@ -5,7 +5,12 @@
 #include <algorithm>
 #include <cstdint>
 #include <iterator>
+#include <string>
 #include <vector>
+
+#include "volt/gfx/extern/stb_image.h"
+
+#include "volt/gfx/GLImport.hpp"
 
 namespace volt::gfx
 {
@@ -19,7 +24,9 @@ namespace volt::gfx
         std::uint_fast32_t width;
         std::uint32_t      spriteID;
 
-    protected:
+        bool   loaded = false;
+        GLuint texGlId;
+
     public:
         Texture();
         Texture(const std::vector<std::uint8_t> &data);
@@ -29,6 +36,14 @@ namespace volt::gfx
         Texture &operator=(const Texture &other);
         Texture &operator=(Texture &&other);
         ~Texture();
+
+        static Texture LoadFromFile(std::string path);
+
+        inline bool IsLoadedIntoVRAM() { return this->loaded; }
+
+        void LoadIntoVRAM();
+
+        void Use(unsigned int unitIndex);
     };
 } // namespace volt::gfx
 
--- volt_gfx/src/volt/gfx/Texture.cpp
+++ volt_gfx/src/volt/gfx/Texture.cpp
@@ -1,8 +1,13 @@
 #include "volt/gfx/Texture.hpp"
+#include "volt/gfx/GLUtilities.hpp"
 
-volt::gfx::Texture::Texture() : height(0), width(0) {}
+#include <cassert>
 
-volt::gfx::Texture::Texture(const volt::gfx::Texture &other)
+using namespace volt::gfx;
+
+Texture::Texture() : height(0), width(0) {}
+
+Texture::Texture(const Texture &other)
 {
     this->height                           = other.height;
     this->width                            = other.width;
@@ -11,25 +16,24 @@ volt::gfx::Texture::Texture(const volt::gfx::Texture &other)
     std::copy(other.imageData.begin(), other.imageData.end(), insert_it);
 }
 
-volt::gfx::Texture::Texture(const std::vector<std::uint8_t> &data)
+Texture::Texture(const std::vector<std::uint8_t> &data)
 {
     std::copy(data.begin(), data.end(), this->imageData.begin());
 }
 
-volt::gfx::Texture::Texture(std::vector<std::uint8_t> &&data)
+Texture::Texture(std::vector<std::uint8_t> &&data)
 {
     this->imageData = std::move(data);
 }
 
-volt::gfx::Texture::Texture(volt::gfx::Texture &&other)
+Texture::Texture(Texture &&other)
 {
     this->height    = other.height;
     this->width     = other.width;
     this->imageData = std::move(other.imageData);
 }
 
-volt::gfx::Texture &
-    volt::gfx::Texture::operator=(const volt::gfx::Texture &other)
+Texture &Texture::operator=(const Texture &other)
 {
     this->height = other.height;
     this->width  = other.width;
@@ -38,7 +42,7 @@ volt::gfx::Texture &
     return *this;
 }
 
-volt::gfx::Texture &volt::gfx::Texture::operator=(volt::gfx::Texture &&other)
+Texture &Texture::operator=(Texture &&other)
 {
     this->height    = other.height;
     this->width     = other.width;
@@ -46,4 +50,50 @@ volt::gfx::Texture &volt::gfx::Texture::operator=(volt::gfx::Texture &&other)
     return *this;
 }
 
-volt::gfx::Texture::~Texture() {}
+volt::gfx::Texture::~Texture() { gl::DeleteTextures(1, &this->texGlId); }
+
+Texture Texture::LoadFromFile(std::string path)
+{
+    int      width = 0, height = 0, colorChannelsCount = 0;
+    stbi_uc *imageData =
+        stbi_load(path.c_str(), &width, &height, &colorChannelsCount, 0);
+    std::size_t dataLen = (std::size_t)(width * height * colorChannelsCount);
+
+    Texture tex   = Texture();
+    tex.width     = width;
+    tex.height    = height;
+    tex.imageData = std::vector<std::uint8_t>();
+    tex.imageData.resize(dataLen);
+    for (std::size_t i = 0; i < dataLen; i++)
+        tex.imageData[i] = imageData[i];
+    stbi_image_free(imageData);
+    return tex;
+}
+
+void Texture::LoadIntoVRAM()
+{
+    GLCall(gl::GenTextures(1, &this->texGlId));
+    GLCall(gl::BindTexture(gl::TEXTURE_2D, this->texGlId));
+    GLCall(gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGBA, this->width,
+                          this->height, 0, gl::RGB, gl::UNSIGNED_BYTE,
+                          this->imageData.data()));
+    GLCall(gl::GenerateMipmap(gl::TEXTURE_2D));
+
+    GLCall(gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S,
+                             gl::CLAMP_TO_BORDER));
+    GLCall(gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T,
+                             gl::CLAMP_TO_BORDER));
+    GLCall(gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER,
+                             gl::NEAREST_MIPMAP_LINEAR));
+    GLCall(
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST));
+
+    this->loaded = true;
+}
+
+void Texture::Use(unsigned int unitIndex)
+{
+    assert(unitIndex < 16);
+    GLCall(gl::ActiveTexture(gl::TEXTURE0 + unitIndex));
+    GLCall(gl::BindTexture(gl::TEXTURE_2D, this->texGlId));
+}
