d58e5a5809256591c8c6fb9ab4ef73cfbcfca269
SirHall <sirhallstein@gmail.com>
2020-02-17 18:55:25 +1000

    Flesh out Material class, adding support for Shader specialization towards rendering objects

--- volt_gfx/include/volt/gfx/Material.hpp
+++ volt_gfx/include/volt/gfx/Material.hpp
@@ -3,17 +3,50 @@
 #define VOLT_GFX_MATERIAL_HPP
 
 #include "volt/gfx/Shader.hpp"
+#include "volt/gfx/Texture.hpp"
 
-class Material
+namespace volt::gfx
 {
-private:
-public:
-    Material();
-    Material(const Material &other);
-    Material(Material &&other);
-    Material &operator=(const Material &other);
-    Material &operator=(Material &&other);
-    ~Material();
-};
 
+    class Material
+    {
+    private:
+        Shader sh;
+
+        // Default uniforms
+        GLint uniformProjection = -1, uniformView = -1, uniformModel = -1,
+              uniformPVM = -1, uniformPV = -1;
+        std::array<GLint, 16> uniformTex = std::array<GLint, 16>();
+
+        void RetreiveDefaultUniforms();
+
+    public:
+        Material(Shader shader);
+        Material(const Material &other);
+        Material &operator=(const Material &other);
+        Material(Material &&other);
+        Material &operator=(Material &&other);
+        ~Material();
+
+        template <typename T>
+        void SetUniform(GLint uniformLoc, const T &value)
+        {
+            sh.SetUniform<T>(uniformLoc, value);
+        }
+
+        bool HasUniformProjection();
+        bool HasUniformView();
+        bool HasUniformModel();
+        bool HasUniformPVM();
+        bool HasUniformPV();
+        bool HasUniformTex(std::uint8_t imageUnit = 0);
+
+        void SetUniformPVM(glm::mat4 projection, glm::mat4 view,
+                           glm::mat4 model);
+        void SetUniformTex(Texture &tex, std::uint8_t imageUnit = 0);
+
+        void SetInUse() const;
+    };
+
+} // namespace volt::gfx
 #endif
--- volt_gfx/src/volt/gfx/Material.cpp
+++ volt_gfx/src/volt/gfx/Material.cpp
@@ -1,13 +1,77 @@
 #include "volt/gfx/Material.hpp"
 
-Material::Material() {}
+using namespace volt::gfx;
 
-Material::Material(const Material &other) {}
+Material::Material(Shader shader) : sh(shader)
+{
+    this->RetreiveDefaultUniforms();
+}
 
-Material::Material(Material &&other) {}
+Material::Material(const Material &other) : sh(other.sh) {}
 
-Material &Material::operator=(const Material &other) { return *this; }
+Material &Material::operator=(const Material &other)
+{
+    this->sh = other.sh;
+    return *this;
+}
 
-Material &Material::operator=(Material &&other) { return *this; }
+Material::Material(Material &&other) : sh(std::move(other.sh)) {}
+
+Material &Material::operator=(Material &&other)
+{
+    this->sh = std::move(other.sh);
+    return *this;
+}
 
 Material::~Material() {}
+
+void Material::RetreiveDefaultUniforms()
+{
+    this->sh.GetUniformLocation("gfxProjection", this->uniformProjection);
+    this->sh.GetUniformLocation("gfxView", this->uniformView);
+    this->sh.GetUniformLocation("gfxModel", this->uniformModel);
+    this->sh.GetUniformLocation("gfxPVM", this->uniformPVM);
+    this->sh.GetUniformLocation("gfxPV", this->uniformPV);
+    // Sampler2D arrays don't work very well in OpenGL, so it isn't supported
+    // here for shaders
+    for (int i = 0; i < this->uniformTex.size(); i++)
+        this->sh.GetUniformLocation("gfxTex" + std::to_string(i),
+                                    this->uniformTex[i]);
+}
+
+#pragma Default Uniforms
+
+bool Material::HasUniformModel() { return this->uniformModel >= 0; }
+bool Material::HasUniformView() { return this->uniformView >= 0; }
+bool Material::HasUniformProjection() { return this->uniformProjection >= 0; }
+bool Material::HasUniformPVM() { return this->uniformPVM >= 0; }
+bool Material::HasUniformPV() { return this->uniformPV >= 0; }
+bool Material::HasUniformTex(std::uint8_t imageUnit)
+{
+    return this->uniformTex[imageUnit] >= 0;
+}
+
+void Material::SetUniformPVM(glm::mat4 projection, glm::mat4 view,
+                             glm::mat4 model)
+{
+    if (this->HasUniformProjection())
+        this->sh.SetUniform(this->uniformProjection, projection);
+    if (this->HasUniformView())
+        this->sh.SetUniform(this->uniformView, view);
+    if (this->HasUniformModel())
+        this->sh.SetUniform(this->uniformModel, model);
+    if (this->HasUniformPVM())
+        this->sh.SetUniform(this->uniformPVM, projection * view * model);
+    if (this->HasUniformPV())
+        this->sh.SetUniform(this->uniformPV, projection * view);
+}
+
+void Material::SetUniformTex(Texture &tex, std::uint8_t imageUnit)
+{
+    if (HasUniformTex(imageUnit))
+        this->sh.SetUniform(this->uniformTex[imageUnit], tex.GetTexID());
+}
+
+#pragma endregion
+
+void Material::SetInUse() const { this->sh.SetInUse(); }
