fc08c709f8b3fed6c09e470e947fc56ad16409d4
SirHall <sirhallstein@gmail.com>
2020-02-17 19:11:17 +1000

    Modify default shaders to use Material class default uniforms

--- volt_gfx/res/shader_frag.glsl
+++ volt_gfx/res/shader_frag.glsl
@@ -7,13 +7,13 @@ in vec2 texCoord;
 
 uniform vec4 u_Color;
 
-uniform sampler2D tex;
+uniform sampler2D tex0;
 
 void main()
 {
-    vec4 col = texture(tex, texCoord);
+    vec4 col = texture(tex0, texCoord);
     if (col.a < 0.85)
         discard;
-    color = texture(tex, texCoord);
+    color = texture(tex0, texCoord);
     // color = (vertPos * 0.5) + 0.5;
 }
--- volt_gfx/res/shader_vert.glsl
+++ volt_gfx/res/shader_vert.glsl
@@ -3,17 +3,14 @@
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec2 aTexCoord;
 
-uniform mat4 model;
-uniform mat4 projection;
-uniform mat4 view;
+uniform mat4 gfxPVM;
 
 out vec3 vertPos;
-
 out vec2 texCoord;
 
 void main()
 {
-    gl_Position = projection * view * model * vec4(position, 1.0);
+    gl_Position = gfxPVM * vec4(position, 1.0);
     vertPos     = position;
     texCoord    = aTexCoord;
 }
