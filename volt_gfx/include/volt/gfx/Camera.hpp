de9a3a8bb764c8a841be3e5348cf0683a26956b9
SirHall <sirhallstein@gmail.com>
2020-02-19 14:22:21 +1000

    Add the Camera class for handling thr render view and projection.

---
+++ volt_gfx/include/volt/gfx/Camera.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef VOLT_GFX_CAMERA_HPP
+#define VOLT_GFX_CAMERA_HPP
+
+#include <glm/glm.hpp>
+
+#include "volt/gfx/GLImport.hpp"
+#include "volt/gfx/Transform.hpp"
+
+namespace volt::gfx
+{
+
+    class Camera
+    {
+    protected:
+        Transform transform;
+
+        mutable bool      projectionDirty = true;
+        mutable glm::mat4 projection;
+        bool              isPerspective = true;
+        float             ratio;
+        float             orthoSize;
+        float             perspectiveFov;
+        float             nearPlane = 0.1f;
+        float             farPlane  = 100.0f;
+
+    public:
+        Camera();
+        Camera(const Camera &other);
+        Camera &operator=(const Camera &other);
+        Camera(Camera &&other);
+        Camera &operator=(Camera &&other);
+        ~Camera();
+
+        Transform const &GetTransform() const;
+        void             SetTransform(Transform v);
+
+        glm::mat4 const &GetProjection() const;
+
+        glm::mat4 GetPV() const;
+
+        bool IsPerspective() const;
+        void SetPerspectiveMode(bool mode = true);
+
+        void SetAspectRatio(float aspectRatio);
+        void SetOrthoSize(float size);
+        void SetPerspectiveFOV(float fov);
+
+        void SetNearFarPlanes(float near, float far);
+    };
+
+} // namespace volt::gfx
+#endif
---
+++ volt_gfx/src/volt/gfx/Camera.cpp
@@ -0,0 +1,70 @@
+#include "volt/gfx/Camera.hpp"
+
+using namespace volt::gfx;
+
+Camera::Camera() {}
+
+Camera::Camera(const Camera &other) {}
+
+Camera &Camera::operator=(const Camera &other) { return *this; }
+
+Camera::Camera(Camera &&other) {}
+
+Camera &Camera::operator=(Camera &&other) { return *this; }
+
+Camera::~Camera() {}
+
+Transform const &Camera::GetTransform() const { return this->transform; }
+void             Camera::SetTransform(Transform v) { this->transform = v; }
+
+glm::mat4 const &Camera::GetProjection() const
+{
+    if (this->projectionDirty)
+    {
+        this->projectionDirty = false;
+        this->projection =
+            this->isPerspective
+                ? glm::perspective(this->perspectiveFov, this->ratio,
+                                   this->nearPlane, this->farPlane)
+                : glm::ortho(0.0f, this->ratio * this->orthoSize,
+                             this->orthoSize, 0.0f, this->nearPlane,
+                             this->farPlane);
+    }
+    return this->projection;
+}
+
+glm::mat4 Camera::GetPV() const
+{
+    return this->transform.GetMatrix() * this->GetProjection();
+}
+
+bool Camera::IsPerspective() const { return this->isPerspective; }
+void Camera::SetPerspectiveMode(bool mode)
+{
+    this->isPerspective   = mode;
+    this->projectionDirty = true;
+}
+
+void Camera::SetAspectRatio(float aspectRatio)
+{
+    this->ratio           = aspectRatio;
+    this->projectionDirty = true;
+}
+
+void Camera::SetOrthoSize(float size)
+{
+    this->orthoSize       = size;
+    this->projectionDirty = true;
+}
+void Camera::SetPerspectiveFOV(float fov)
+{
+    this->perspectiveFov  = fov;
+    this->projectionDirty = true;
+}
+
+void Camera::SetNearFarPlanes(float near, float far)
+{
+    this->nearPlane       = near;
+    this->farPlane        = far;
+    this->projectionDirty = true;
+}
