5653dc3cba70253be4db78fda076603ba994522a
SirHall <sirhallstein@gmail.com>
2020-02-19 14:23:13 +1000

    Update gfx.hpp with RenderObject and Camera headers

--- volt_gfx/include/volt/gfx.hpp
+++ volt_gfx/include/volt/gfx.hpp
@@ -2,12 +2,14 @@
 #ifndef VOLT_GFX_HPP
 #define VOLT_GFX_HPP
 
+#include "volt/gfx/Camera.hpp"
 #include "volt/gfx/Framebuffer.hpp"
 #include "volt/gfx/GLImport.hpp"
 #include "volt/gfx/GLUtilities.hpp"
 #include "volt/gfx/Image.hpp"
 #include "volt/gfx/Material.hpp"
 #include "volt/gfx/Mesh.hpp"
+#include "volt/gfx/RenderObject.hpp"
 #include "volt/gfx/Renderer.hpp"
 #include "volt/gfx/ShadeletSource.hpp"
 #include "volt/gfx/Shader.hpp"
