8a95edfb3d1ab626f788c154e954efb10825135c
SirHall <sirhallstein@gmail.com>
2020-07-10 18:35:56 +1000

    Rename 'Vertex' to 'MeshVertex' and allow it to use upcoming VAOGen attribute builder and VBO builder

---
+++ volt_gfx/include/volt/gfx/MeshVertex.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef VOLT_GFX_MESHVERTEX_HPP
+#define VOLT_GFX_MESHVERTEX_HPP
+
+#include "volt/gfx/GLImport.hpp"
+#include "volt/gfx/VBO.hpp"
+
+#include <glm/glm.hpp>
+
+namespace volt::gfx
+{
+    struct MeshVertex
+    {
+    private:
+    protected:
+    public:
+        MeshVertex();
+        MeshVertex(GLfloat pos_x, GLfloat pos_y, GLfloat pos_z,
+                   GLfloat uv_x = 0.0f, GLfloat uv_y = 0.0f);
+        MeshVertex(const MeshVertex &other);
+        MeshVertex &operator=(const MeshVertex &other);
+        ~MeshVertex();
+
+        glm::vec3 position;
+        glm::vec2 uv;
+    };
+
+    template <>
+    void GenVBO<MeshVertex>(GLuint vbo);
+} // namespace volt::gfx
+
+#endif
---
+++ volt_gfx/src/volt/gfx/MeshVertex.cpp
@@ -0,0 +1,56 @@
+#include "volt/gfx/MeshVertex.hpp"
+#include "volt/gfx/GLUtilities.hpp"
+#include "volt/gfx/VAOGen.hpp"
+
+#include <iostream>
+
+using namespace volt::gfx;
+
+MeshVertex::MeshVertex() {}
+
+MeshVertex::MeshVertex(GLfloat pos_x, GLfloat pos_y, GLfloat pos_z,
+                       GLfloat uv_x, GLfloat uv_y)
+{
+    position[0] = pos_x;
+    position[1] = pos_y;
+    position[2] = pos_z;
+
+    uv[0] = uv_x;
+    uv[1] = uv_y;
+}
+
+MeshVertex::MeshVertex(const MeshVertex &other)
+{
+    this->position[0] = other.position[0];
+    this->position[1] = other.position[1];
+    this->position[2] = other.position[2];
+
+    this->uv[0] = other.uv[0];
+    this->uv[1] = other.uv[1];
+}
+
+MeshVertex &MeshVertex::operator=(const MeshVertex &other)
+{
+    this->position[0] = other.position[0];
+    this->position[1] = other.position[1];
+    this->position[2] = other.position[2];
+
+    this->uv[0] = other.uv[0];
+    this->uv[1] = other.uv[1];
+
+    return *this;
+}
+
+MeshVertex::~MeshVertex() {}
+
+template <>
+void volt::gfx::GenVBO<MeshVertex>(GLuint vbo)
+{
+    GLuint vertIndex = 0;
+
+    vertIndex = VAOGen::GenVertAttrib<glm::vec3>(
+        "gfxPos", vertIndex, sizeof(MeshVertex),
+        offsetof(MeshVertex, position));
+    vertIndex = VAOGen::GenVertAttrib<glm::vec2>(
+        "gfxUV", vertIndex, sizeof(MeshVertex), offsetof(MeshVertex, uv));
+}
