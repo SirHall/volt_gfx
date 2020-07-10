a8b2f82f0bef69f27ccc059e77309750992e3249
SirHall <sirhallstein@gmail.com>
2020-07-10 18:36:28 +1000

    Setuo VAOGen to assist in generating VAO's

---
+++ volt_gfx/include/volt/gfx/VAOGen.hpp
@@ -0,0 +1,138 @@
+#pragma once
+#ifndef VOLT_GFX_VAOGEN_HPP
+#define VOLT_GFX_VAOGEN_HPP
+
+#include "volt/gfx/GLImport.hpp"
+#include "volt/gfx/GLUtilities.hpp"
+
+#include <glm/glm.hpp>
+
+#include <string>
+
+namespace volt::gfx::VAOGen
+{
+
+    GLuint GenerateVAO();
+
+    // Returns next vertex attribute index
+    template <typename T>
+    GLuint GenVertAttrib(std::string const &name, GLuint startIndex,
+                         std::size_t stride, std::size_t offset,
+                         bool instanced = false)
+    { // TODO: Ensure that this can only run with integral types
+        GLCall(glEnableVertexAttribArray(startIndex));
+        GLCall(glVertexAttribPointer(startIndex, 1, GetGlType<T>(), GL_FALSE,
+                                     stride, reinterpret_cast<void *>(offset)));
+        if (instanced)
+            glVertexAttribDivisor(startIndex, 1);
+        return startIndex + 1;
+    }
+
+    // Declare vertex attribute generators for specialized types
+    // // Integers
+    // template <>
+    // GLuint GenVertAttrib<glm::int8>(std::string const &name, GLuint
+    // startIndex,
+    //                                 std::size_t stride, std::size_t offset,
+    //                                 bool instanced);
+    // template <>
+    // GLuint GenVertAttrib<glm::int16>(std::string const &name, GLuint
+    // startIndex,
+    //                                  std::size_t stride, std::size_t offset,
+    //                                  bool instanced);
+    // template <>
+    // GLuint GenVertAttrib<glm::int32>(std::string const &name, GLuint
+    // startIndex,
+    //                                  std::size_t stride, std::size_t offset,
+    //                                  bool instanced);
+    // template <>
+    // GLuint GenVertAttrib<glm::int64>(std::string const &name, GLuint
+    // startIndex,
+    //                                  std::size_t stride, std::size_t offset,
+    //                                  bool instanced);
+    // template <>
+    // GLuint GenVertAttrib<glm::uint8>(std::string const &name, GLuint
+    // startIndex,
+    //                                  std::size_t stride, std::size_t offset,
+    //                                  bool instanced);
+    // template <>
+    // GLuint GenVertAttrib<glm::uint16>(std::string const &name,
+    //                                   GLuint startIndex, std::size_t stride,
+    //                                   std::size_t offset, bool instanced);
+    // template <>
+    // GLuint GenVertAttrib<glm::uint32>(std::string const &name,
+    //                                   GLuint startIndex, std::size_t stride,
+    //                                   std::size_t offset, bool instanced);
+    // template <>
+    // GLuint GenVertAttrib<glm::uint64>(std::string const &name,
+    //                                   GLuint startIndex, std::size_t stride,
+    //                                   std::size_t offset, bool instanced);
+
+    // // Floating point
+    // template <>
+    // GLuint GenVertAttrib<glm::float32>(std::string const &name,
+    //                                    GLuint startIndex, std::size_t stride,
+    //                                    std::size_t offset, bool instanced);
+    // template <>
+    // GLuint GenVertAttrib<glm::float64>(std::string const &name,
+    //                                    GLuint startIndex, std::size_t stride,
+    //                                    std::size_t offset, bool instanced);
+
+    // Vectors
+    template <>
+    GLuint GenVertAttrib<glm::vec1>(std::string const &name, GLuint startIndex,
+                                    std::size_t stride, std::size_t offset,
+                                    bool instanced);
+    template <>
+    GLuint GenVertAttrib<glm::vec2>(std::string const &name, GLuint startIndex,
+                                    std::size_t stride, std::size_t offset,
+                                    bool instanced);
+    template <>
+    GLuint GenVertAttrib<glm::vec3>(std::string const &name, GLuint startIndex,
+                                    std::size_t stride, std::size_t offset,
+                                    bool instanced);
+    template <>
+    GLuint GenVertAttrib<glm::vec4>(std::string const &name, GLuint startIndex,
+                                    std::size_t stride, std::size_t offset,
+                                    bool instanced);
+
+    // Matrices
+    template <>
+    GLuint GenVertAttrib<glm::mat2x2>(std::string const &name,
+                                      GLuint startIndex, std::size_t stride,
+                                      std::size_t offset, bool instanced);
+    template <>
+    GLuint GenVertAttrib<glm::mat2x3>(std::string const &name,
+                                      GLuint startIndex, std::size_t stride,
+                                      std::size_t offset, bool instanced);
+    template <>
+    GLuint GenVertAttrib<glm::mat2x4>(std::string const &name,
+                                      GLuint startIndex, std::size_t stride,
+                                      std::size_t offset, bool instanced);
+    template <>
+    GLuint GenVertAttrib<glm::mat3x2>(std::string const &name,
+                                      GLuint startIndex, std::size_t stride,
+                                      std::size_t offset, bool instanced);
+    template <>
+    GLuint GenVertAttrib<glm::mat3x3>(std::string const &name,
+                                      GLuint startIndex, std::size_t stride,
+                                      std::size_t offset, bool instanced);
+    template <>
+    GLuint GenVertAttrib<glm::mat3x4>(std::string const &name,
+                                      GLuint startIndex, std::size_t stride,
+                                      std::size_t offset, bool instanced);
+    template <>
+    GLuint GenVertAttrib<glm::mat4x2>(std::string const &name,
+                                      GLuint startIndex, std::size_t stride,
+                                      std::size_t offset, bool instanced);
+    template <>
+    GLuint GenVertAttrib<glm::mat4x3>(std::string const &name,
+                                      GLuint startIndex, std::size_t stride,
+                                      std::size_t offset, bool instanced);
+    template <>
+    GLuint GenVertAttrib<glm::mat4x4>(std::string const &name,
+                                      GLuint startIndex, std::size_t stride,
+                                      std::size_t offset, bool instanced);
+
+} // namespace volt::gfx::VAOGen
+#endif
---
+++ volt_gfx/src/volt/gfx/VAOGen.cpp
@@ -0,0 +1,160 @@
+#include "volt/gfx/VAOGen.hpp"
+
+using namespace volt::gfx::VAOGen;
+
+GLuint GenerateVAO()
+{
+    GLuint vao = 0;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
+    return vao;
+}
+
+GLuint GenNVecVertAttrib(std::string const &name, GLuint startIndex,
+                         std::size_t stride, std::size_t offset, bool instanced,
+                         std::size_t size)
+{
+    GLCall(glEnableVertexAttribArray(startIndex));
+    GLCall(glVertexAttribPointer(startIndex, size, GetGlType<float>(), GL_FALSE,
+                                 stride, reinterpret_cast<void *>(offset)));
+    if (instanced)
+        glVertexAttribDivisor(startIndex, 1);
+    return startIndex + 1;
+}
+
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::vec1>(std::string const &name,
+                                                   GLuint      startIndex,
+                                                   std::size_t stride,
+                                                   std::size_t offset,
+                                                   bool        instanced)
+{
+    return GenNVecVertAttrib(name, startIndex, stride, offset, instanced, 1);
+}
+
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::vec2>(std::string const &name,
+                                                   GLuint      startIndex,
+                                                   std::size_t stride,
+                                                   std::size_t offset,
+                                                   bool        instanced)
+{
+    return GenNVecVertAttrib(name, startIndex, stride, offset, instanced, 2);
+}
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::vec3>(std::string const &name,
+                                                   GLuint      startIndex,
+                                                   std::size_t stride,
+                                                   std::size_t offset,
+                                                   bool        instanced)
+{
+    return GenNVecVertAttrib(name, startIndex, stride, offset, instanced, 3);
+}
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::vec4>(std::string const &name,
+                                                   GLuint      startIndex,
+                                                   std::size_t stride,
+                                                   std::size_t offset,
+                                                   bool        instanced)
+{
+    return GenNVecVertAttrib(name, startIndex, stride, offset, instanced, 4);
+}
+
+GLuint GemNMatVertAttrib(std::string const &name, GLuint startIndex,
+                         std::size_t stride, std::size_t offset, bool instanced,
+                         std::size_t I, std::size_t J)
+{
+    for (std::size_t i = 0; i < I; i++)
+    {
+        GLCall(glEnableVertexAttribArray(startIndex));
+        GLCall(glVertexAttribPointer(
+            startIndex, J, GetGlType<float>(), GL_FALSE, stride,
+            reinterpret_cast<void *>(offset + (i * J))));
+        if (instanced)
+            glVertexAttribDivisor(startIndex, 1);
+        startIndex++;
+    }
+    return startIndex;
+}
+
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::mat2x2>(std::string const &name,
+                                                     GLuint      startIndex,
+                                                     std::size_t stride,
+                                                     std::size_t offset,
+                                                     bool        instanced)
+{
+    return GemNMatVertAttrib(name, startIndex, stride, offset, instanced, 2, 2);
+}
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::mat2x3>(std::string const &name,
+                                                     GLuint      startIndex,
+                                                     std::size_t stride,
+                                                     std::size_t offset,
+                                                     bool        instanced)
+{
+    return GemNMatVertAttrib(name, startIndex, stride, offset, instanced, 2, 3);
+}
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::mat2x4>(std::string const &name,
+                                                     GLuint      startIndex,
+                                                     std::size_t stride,
+                                                     std::size_t offset,
+                                                     bool        instanced)
+{
+    return GemNMatVertAttrib(name, startIndex, stride, offset, instanced, 2, 4);
+}
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::mat3x2>(std::string const &name,
+                                                     GLuint      startIndex,
+                                                     std::size_t stride,
+                                                     std::size_t offset,
+                                                     bool        instanced)
+{
+    return GemNMatVertAttrib(name, startIndex, stride, offset, instanced, 3, 2);
+}
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::mat3x3>(std::string const &name,
+                                                     GLuint      startIndex,
+                                                     std::size_t stride,
+                                                     std::size_t offset,
+                                                     bool        instanced)
+{
+    return GemNMatVertAttrib(name, startIndex, stride, offset, instanced, 3, 3);
+}
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::mat3x4>(std::string const &name,
+                                                     GLuint      startIndex,
+                                                     std::size_t stride,
+                                                     std::size_t offset,
+                                                     bool        instanced)
+{
+    return GemNMatVertAttrib(name, startIndex, stride, offset, instanced, 3, 4);
+}
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::mat4x2>(std::string const &name,
+                                                     GLuint      startIndex,
+                                                     std::size_t stride,
+                                                     std::size_t offset,
+                                                     bool        instanced)
+{
+    return GemNMatVertAttrib(name, startIndex, stride, offset, instanced, 4, 2);
+}
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::mat4x3>(std::string const &name,
+                                                     GLuint      startIndex,
+                                                     std::size_t stride,
+                                                     std::size_t offset,
+                                                     bool        instanced)
+{
+    return GemNMatVertAttrib(name, startIndex, stride, offset, instanced, 4, 3);
+}
+template <>
+GLuint volt::gfx::VAOGen::GenVertAttrib<glm::mat4x4>(std::string const &name,
+                                                     GLuint      startIndex,
+                                                     std::size_t stride,
+                                                     std::size_t offset,
+                                                     bool        instanced)
+{
+    return GemNMatVertAttrib(name, startIndex, stride, offset, instanced, 4, 4);
+}
