768b95bd02f2182fd733d59553c720a14631965b
SirHall <sirhallstein@gmail.com>
2020-07-12 21:04:10 +1000

    Fill out VBO class

--- volt_gfx/include/volt/gfx/VBO.hpp
+++ volt_gfx/include/volt/gfx/VBO.hpp
@@ -12,30 +12,47 @@
 namespace volt::gfx
 {
     template <typename T>
-    void GenVBO(GLuint vbo);
+    GLuint GenVBO(GLuint vbo, GLuint attribIndex);
 
     template <typename T, GLenum VBOUsage>
     class VBO : public Buffer<T, GL_ARRAY_BUFFER, VBOUsage>
     {
     private:
+        GLuint initAttribIndex;
+
     public:
         // TODO: Only allow this class to be instantiated withint volt_gfx
-        VBO(GLuint vaoID) : Buffer(vaoID) { GenVBO<T>(this->vbo); }
+        VBO(GLuint vaoID, GLuint &attribIndex)
+            : Buffer(vaoID), initAttribIndex(attribIndex)
+        {
+            this->Bind();
+            attribIndex = GenVBO<T>(this->vbo, attribIndex);
+        }
 
-        VBO(const VBO &other) : Buffer(other) { GenVBO<T>(this->vbo); }
+        VBO(const VBO &other)
+            : Buffer(other), initAttribIndex(other.initAttribIndex)
+        {
+            this->Bind();
+            GenVBO<T>(this->vbo, this->initAttribIndex);
+        }
 
         VBO &operator=(const VBO &other)
         {
-            Buffer::operator=(other);
-            GenVBO<T>(this->vbo);
+            Buffer::operator      =(other);
+            this->initAttribIndex = other.initAttribIndex;
+            this->Bind();
+            GenVBO<T>(this->vbo, this->initAttribIndex);
             return *this;
         }
 
-        VBO(VBO &&other) : Buffer(other) {}
+        VBO(VBO &&other) : Buffer(other), initAttribIndex(other.initAttribIndex)
+        {
+        }
 
         VBO &operator=(VBO &&other)
         {
-            Buffer::operator=(other);
+            Buffer::operator      =(other);
+            this->initAttribIndex = other.initAttribIndex;
             return *this;
         }
 
