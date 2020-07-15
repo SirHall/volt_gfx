6bb34bc2bd38dcb06a00d6ecdcbeb51089cf5c09
SirHall <sirhallstein@gmail.com>
2020-07-15 18:19:56 +1000

    Update shader_vert.glsl to use new named attributes

--- volt_gfx/res/shader_vert.glsl
+++ volt_gfx/res/shader_vert.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec2 aTexCoord;
+in vec3 gfxPos;
+in vec2 gfxUV;
 
 uniform mat4 gfxPVM;
 
@@ -10,7 +10,7 @@ out vec2 texCoord;
 
 void main()
 {
-    gl_Position = gfxPVM * vec4(position, 1.0);
-    vertPos     = position;
-    texCoord    = aTexCoord;
+    gl_Position = gfxPVM * vec4(gfxPos, 1.0);
+    vertPos     = gfxPos;
+    texCoord    = gfxUV;
 }
