6bdc9761c8e1f8a9139b45f7e129920c78a2d60d
SirHall <sirhallstein@gmail.com>
2020-07-25 18:37:48 +1000

    List new header filed in gfx.hpp

--- volt_gfx/include/volt/gfx.hpp
+++ volt_gfx/include/volt/gfx.hpp
@@ -7,6 +7,7 @@
 #include "volt/gfx/Framebuffer.hpp"
 #include "volt/gfx/GLImport.hpp"
 #include "volt/gfx/GLUtilities.hpp"
+#include "volt/gfx/IBO.hpp"
 #include "volt/gfx/Image.hpp"
 #include "volt/gfx/Material.hpp"
 #include "volt/gfx/Mesh.hpp"
@@ -19,6 +20,9 @@
 #include "volt/gfx/Sprite.hpp"
 #include "volt/gfx/Texture.hpp"
 #include "volt/gfx/Transform.hpp"
+#include "volt/gfx/VAO.hpp"
+#include "volt/gfx/VAOGen.hpp"
+#include "volt/gfx/VBO.hpp"
 #include "volt/gfx/global_events/GFXEvents.hpp"
 
 #endif
